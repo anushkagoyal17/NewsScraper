49f3ad3ce6648e974877480aae46a9a417ad55e7
anushkagoyal17 <124656119+anushkagoyal17@users.noreply.github.com>
2024-04-26 00:11:19 +0530

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,29 +1,12 @@
-from robot.api import TestData
+from robot.api.deco import keyword
 from APNewsScraper import APNewsScraper
 from config import SEARCH_PHRASE, NEW_CATEGORY, NUM_MONTHS
 
-def get_test_cases():
-    # Create a test suite
-    suite = TestData(parent=None, source="")
-
-    # Define a test case
-    test_case = suite.tests.create(name="Scrape APNews")
-
-    # Define the test steps
-    test_case.keywords.create(name="Initialize APNewsScraper", args=[SEARCH_PHRASE, NEW_CATEGORY, NUM_MONTHS])
-    test_case.keywords.create(name="Scrape")
-
-    return suite
-
-# from robot.api.deco import keyword
-# from APNewsScraper import APNewsScraper
-# from config import SEARCH_PHRASE, NEW_CATEGORY, NUM_MONTHS
-
-# @keyword('Scrape APNews')
-# def scrape_apnews():
-#     search_phrase = SEARCH_PHRASE
-#     news_category = NEW_CATEGORY
-#     num_months = NUM_MONTHS
+@keyword('Scrape APNews')
+def scrape_apnews():
+    search_phrase = SEARCH_PHRASE
+    news_category = NEW_CATEGORY
+    num_months = NUM_MONTHS
     
-#     scraper = APNewsScraper(search_phrase, news_category, num_months)
-#     scraper.scrape()
+    scraper = APNewsScraper(search_phrase, news_category, num_months)
+    scraper.scrape()
