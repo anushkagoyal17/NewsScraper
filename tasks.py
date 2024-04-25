d3f640b02b0ec87b583bf5990c858e3dd7e852ec
anushkagoyal17 <124656119+anushkagoyal17@users.noreply.github.com>
2024-04-26 00:01:44 +0530

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,9 +1,10 @@
 from APNewsScraper import APNewsScraper
+from config import SEARCH_PHRASE, NEW_CATEGORY, NUM_MONTHS
 
 def Scrape_APNews():
-    search_phrase = "${SEARCH_PHRASE}"
-    news_category = "${NEW_CATEGORY}"
-    num_months = ${NUM_MONTHS}
+    search_phrase = SEARCH_PHRASE
+    news_category = NEW_CATEGORY
+    num_months = NUM_MONTHS
     
     scraper = APNewsScraper(search_phrase, news_category, num_months)
     scraper.scrape()
