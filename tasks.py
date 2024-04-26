4eade0e7d00eddb7fc9a6b37d03b453986fece31
anushkagoyal17 <124656119+anushkagoyal17@users.noreply.github.com>
2024-04-26 09:09:28 +0530

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -2,7 +2,7 @@ from robot.api.deco import keyword
 from APNewsScraper import APNewsScraper
 from config import SEARCH_PHRASE, NEW_CATEGORY, NUM_MONTHS
 
-
+@task
 @keyword("Scrape APNews")
 def scrape_apnews(search_phrase=SEARCH_PHRASE, news_category=NEW_CATEGORY, num_months=NUM_MONTHS):
     """Scrapes news data from APNews based on provided search parameters.
